2e02be9d6c6716a7895538680ca9ae3da7f6fef3
hornbuck <124624417+hornbuck@users.noreply.github.com>
2023-05-20 22:22:21 -0700

    Fixes Beaver, added AI background, and implemented music

--- beavUp/gpt.py
+++ beavUp/gpt.py
@@ -1,41 +1,42 @@
 import os
 import openai
 import requests
+
 openai.api_key = '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'
 
-#FUNCTION: NPC Dialogue Generation
-#INPUTS: Type, Name, Personality Attributes
-#OUTPUTS: AI-Generated Phrase
-def dialogue(npc, name, personality):
 
+# FUNCTION: NPC Dialogue Generation
+# INPUTS: Type, Name, Personality Attributes
+# OUTPUTS: AI-Generated Phrase
+def dialogue(npc, name, personality):
     result = ""
 
-    #Generates a prompt
+    # Generates a prompt
     completion = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-        {"role": "user", "content": "You are " + str(name) + ", a " + str(personality) + str(npc) + "that gives directions to the "
-                                    "top of the of the mountain to our protagonist, Beavy. Output some text of 20 words or less "
-                                    "telling him to go to the top of the mountain, remember your personality!"}
-      ]
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user",
+             "content": "You are " + str(name) + ", a " + str(personality) + str(npc) + "that gives directions to the "
+                                                                                        "top of the of the mountain to our protagonist, Beavy. Output some text of 20 words or less "
+                                                                                        "telling him to go to the top of the mountain, remember your personality!"}
+        ]
     )
 
-    #Parses response
+    # Parses response
     response = completion.choices[0].message
     result = response["content"]
 
     return result
-    
 
-#FUNCTION: Image Generation
-#INPUTS: Prompt, preferred path of image
-#OUTPUTS: None - void
+
+# FUNCTION: Image Generation
+# INPUTS: Prompt, preferred path of image
+# OUTPUTS: None - void
 def newImage(subject, path):
-
     response = openai.Image.create(
-      prompt=str(subject),
-      n=1,
-      size="1024x720"
+        prompt=str(subject),
+        n=1,
+        size="1024x1024"
     )
 
     image_url = response['data'][0]['url']
---
+++ beavUp/player.py
@@ -0,0 +1,55 @@
+import pygame
+
+# Loads sprite images
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 720
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+beavy_image = pygame.image.load('assets/beavy.png').convert_alpha()
+beavy_opp = pygame.image.load('assets/beavy_opp.png').convert_alpha()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        
+        self.image = pygame.transform.scale(beavy_image, (150, 150))
+        self.rect = self.image.get_rect(topleft = pos)
+
+        #player movement
+        self.direction = pygame.math.Vector2(0, 0)
+        self.speed = 5
+        self.gravity = 0.4
+        self.jump_speed = -12
+        self.is_on_ground = False
+
+    def import_char_assets(self):
+        character_path = '/assets/beavy.png'
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        #Changes direction
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            #Sprite change
+            self.image = pygame.transform.scale(beavy_image, (150, 150))
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            #Sprite change
+            self.image = pygame.transform.scale(beavy_opp, (150, 150))
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+        if keys[pygame.K_SPACE] or keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.jump()
+
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+        self.is_on_ground = False
+
+    def jump(self):
+        if self.is_on_ground:
+            self.direction.y = self.jump_speed
+
+    def update(self):
+        self.get_input()
