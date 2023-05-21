7f09742d66ce11c846d6f0842c7d1d14efe096b4
hornbuck <124624417+hornbuck@users.noreply.github.com>
2023-05-20 23:05:16 -0700

    Platformer Texture

--- beavUp/tiles.py
+++ beavUp/tiles.py
@@ -1,10 +1,17 @@
 import pygame
 
+# Loads sprite images
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 720
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+flag_image = pygame.image.load('assets/platform.png').convert_alpha()
+
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
-        self.image = pygame.Surface((size, size))
-        self.image.fill((255, 255, 255))
+        #self.image = pygame.Surface((size, size))
+        #self.image.fill((255, 255, 255))
+        self.image = pygame.transform.scale(flag_image, (55, 55))
         self.rect = self.image.get_rect(topleft = pos)
 
     def update(self, x_shift, y_shift):
