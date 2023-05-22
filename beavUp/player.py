af70900cebd35cd003a42ac62c7ce9117b1bfe38
hornbuck <124624417+hornbuck@users.noreply.github.com>
2023-05-21 20:18:37 -0700

    New Version

--- beavUp/player.py
+++ beavUp/player.py
@@ -7,19 +7,24 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 beavy_image = pygame.image.load('assets/beavy.png').convert_alpha()
 beavy_opp = pygame.image.load('assets/beavy_opp.png').convert_alpha()
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        
-        self.image = pygame.transform.scale(beavy_image, (55, 55))
-        self.rect = self.image.get_rect(topleft = pos)
 
-        #player movement
+        self.image = pygame.transform.scale(beavy_image, (55, 55))
+        self.rect = self.image.get_rect(topleft=pos)
+
+        # player movement
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 5
         self.gravity = 0.4
         self.jump_speed = -12
         self.is_on_ground = False
+        self.initial_pos = pos
+
+    def reset_pos(self):
+        self.rect.topleft = self.initial_pos
 
     def import_char_assets(self):
         character_path = '/assets/beavy.png'
@@ -27,13 +32,13 @@ class Player(pygame.sprite.Sprite):
     def get_input(self):
         keys = pygame.key.get_pressed()
 
-        #Changes direction
+        # Changes direction
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            #Sprite change
+            # Sprite change
             self.image = pygame.transform.scale(beavy_image, (55, 55))
             self.direction.x = 1
         elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            #Sprite change
+            # Sprite change
             self.image = pygame.transform.scale(beavy_opp, (55, 55))
             self.direction.x = -1
         else:
@@ -45,11 +50,15 @@ class Player(pygame.sprite.Sprite):
     def apply_gravity(self):
         self.direction.y += self.gravity
         self.rect.y += self.direction.y
-        self.is_on_ground = False
+
+        if not self.is_on_ground:
+            self.is_on_ground = False
 
     def jump(self):
         if self.is_on_ground:
             self.direction.y = self.jump_speed
+            self.is_on_ground = False
 
     def update(self):
         self.get_input()
+        self.apply_gravity()
