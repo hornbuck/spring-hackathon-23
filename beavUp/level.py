d8399fd8da652ef471e1639a8600c9b0a9b77fd6
hornbuck <124624417+hornbuck@users.noreply.github.com>
2023-05-21 20:19:49 -0700

    Add files via upload

--- beavUp/level.py
+++ beavUp/level.py
@@ -1,4 +1,6 @@
 import pygame
+import setuptools.errors
+
 from tiles import Tile
 from settings import tile_size, SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
@@ -46,30 +48,15 @@ class Level:
         player_x = player.rect.centerx
         direction_x = player.direction.x
 
-        if player_x < SCREEN_WIDTH // 4 and direction_x < 0:
-            self.world_shift_x = 5
+        if player_x < SCREEN_WIDTH / 4 and direction_x < 0:
+            self.world_shift_x = 8
             player.speed = 0
-        elif player_x > SCREEN_WIDTH - (SCREEN_WIDTH // 4) and direction_x > 0:
-            self.world_shift_x = -5
+        elif player_x > SCREEN_WIDTH - (SCREEN_WIDTH / 4) and direction_x > 0:
+            self.world_shift_x = -8
             player.speed = 0
         else:
             self.world_shift_x = 0
-            player.speed = 5
-
-    def scroll_y(self):
-        player = self.player.sprite
-        player_y = player.rect.centery
-        direction_y = player.direction.y
-
-        if player_y < SCREEN_HEIGHT // 2 and direction_y < 0:
-            self.world_shift_y = 5
-            player.speed = 0
-        elif player_y > SCREEN_HEIGHT - (SCREEN_HEIGHT // 2) and direction_y > 0:
-            self.world_shift_y = -5
-            player.speed = 0
-        else:
-            self.world_shift_y = 0
-            player.speed = 5
+            player.speed = 3
 
     def horizontal_move_collision(self):
         player = self.player.sprite
@@ -102,7 +89,6 @@ class Level:
         self.tiles.update(self.world_shift_x, self.world_shift_y)
         self.tiles.draw(self.display_surface)
         self.scroll_x()
-        self.scroll_y()
 
         #npc
         self.npcs.update(self.world_shift_x, self.world_shift_y)
@@ -116,6 +102,9 @@ class Level:
         self.horizontal_move_collision()
         self.player.draw(self.display_surface)
 
+        #bring player back to og position if out of bounds
+        if self.player.sprite.rect.y > SCREEN_HEIGHT:
+            self.player.sprite.reset_pos()
         #lion
         self.lion.update(self.world_shift_x, self.world_shift_y)
         self.vertical_move_collision()
